79dbd749b269e207ad20a8675ca02816d82c71fe
Haroon Tahir <noono14252@gmail.com>
2022-05-11 01:18:16 +0500

    Dependencies Added

--- app/src/main/java/com/example/library_management_system/view/LoginActivity.kt
+++ app/src/main/java/com/example/library_management_system/view/LoginActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import com.example.library_management_system.R
 
 class LoginActivity : AppCompatActivity() {
+//	https://console.firebase.google.com/u/0/project/library-management-syste-842a9/database/library-management-syste-842a9-default-rtdb/data/~2F
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.login_activity)
--- app/src/main/java/com/example/library_management_system/view/SignupActivity.kt
+++ app/src/main/java/com/example/library_management_system/view/SignupActivity.kt
@@ -2,11 +2,21 @@ package com.example.library_management_system.view
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
 import com.example.library_management_system.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignupActivity : AppCompatActivity() {
+
+	private lateinit var auth: FirebaseAuth
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.signup_activity)
+
+
 	}
 }
