a6960b7c0db394fd0346b88dcae03e51ab10f5bf
Haroon Tahir <noono14252@gmail.com>
2022-05-11 18:58:21 +0500

    login_activity.xml Changes Added

--- app/src/main/java/com/example/library_management_system/view/LoginActivity.kt
+++ app/src/main/java/com/example/library_management_system/view/LoginActivity.kt
@@ -1,13 +1,20 @@
 package com.example.library_management_system.view
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.example.library_management_system.R
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 //	https://console.firebase.google.com/u/0/project/library-management-syste-842a9/database/library-management-syste-842a9-default-rtdb/data/~2F
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.login_activity)
+
+		var auth = FirebaseAuth.getInstance()
+	}
+
+	override fun onStart() {
+		super.onStart()
 	}
 }
--- app/src/main/java/com/example/library_management_system/view/SignupActivity.kt
+++ app/src/main/java/com/example/library_management_system/view/SignupActivity.kt
@@ -1,13 +1,9 @@
 package com.example.library_management_system.view
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.library_management_system.R
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 class SignupActivity : AppCompatActivity() {
 
